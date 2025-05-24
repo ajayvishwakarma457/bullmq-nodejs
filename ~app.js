ad02117d62a11fde3d404af35dc075c1e4705576
Ajay <ajayvishwakarma457@gmail.com>
2025-05-24 21:41:53 +0530

    changes

--- ~app.js
+++ ~app.js
@@ -1,22 +1,24 @@
 import express from 'express';
 
 import { createBullBoard } from '@bull-board/api';
-import { BullMQAdapter } from '@bull-board/api/bullMQAdapter.js';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter.js'; 
 import { ExpressAdapter } from '@bull-board/express';
 
-import emailQueue from './queues/emailQueue.js'; 
-import emailRoutes from './routes/emailRoutes.js';
+
 
 const app = express();
 const PORT = 3000;
+
+import emailRoutes from './routes/emailRoutes.js';
 
 const serverAdapter = new ExpressAdapter();
 serverAdapter.setBasePath('/admin/queues');
 
 createBullBoard({
-  queues: [new BullMQAdapter(emailQueue)], 
+  queues: [new BullMQAdapter(emailRoutes)],
   serverAdapter,
 });
+
 
 app.use(express.json());
 app.use('/api', emailRoutes);
@@ -24,5 +26,6 @@ app.use('/admin/queues', serverAdapter.getRouter());
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
   console.log(`Bull Board is available at http://localhost:${PORT}/admin/queues`);
 });
